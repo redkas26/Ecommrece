fc80cf313494b0838093295cc3c91c6e57c2bfbe
redkas <redkas26@gmail.com>
2023-12-08 10:41:57 +0100

    modification files

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -12,7 +12,7 @@ function Cart() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Add to Cart</Button>
       </Card.Body>
     </Card>
   )
